033a930d6293e5b54bf1d57a2ac97b5ef65edf80
Restyled.io <commits@restyled.io>
2022-08-07 14:04:25 +0000

    Restyled by prettier

--- assets/scripts/activate-db-links.js
+++ assets/scripts/activate-db-links.js
@@ -2,7 +2,7 @@
 var header = document.getElementsByClassName("dbLink-container")[0];
 var btns = header.getElementsByClassName("dbLink");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("dbLink--active");
     current[0].className = current[0].className.replace(" dbLink--active", "");
     this.className += " dbLink--active";
--- assets/scripts/activate-steps.js
+++ assets/scripts/activate-steps.js
@@ -2,7 +2,7 @@
 var header = document.getElementsByClassName("status-container")[0];
 var btns = header.getElementsByClassName("status");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("status--active");
     current[0].className = current[0].className.replace(" status--active", "");
     this.className += " status--active";
