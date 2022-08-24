871a7bc733ef46bf6c4bf62f908bddf83337448d
domster4444 <kshitiz.shah@deerwalk.edu.np>
2022-08-24 07:33:46 -0500

    add dropdown in nav in all dashboard screens, add dynamic SN in academic info bca & csit

---
+++ assets/scripts/dropdown-nav.js
@@ -0,0 +1,11 @@
+// drop down script
+function dropDown(e) {
+  let dropdownContent = e.nextElementSibling;
+  if (e.classList.contains("active-dropdown")) {
+    dropdownContent.style.maxHeight = 0;
+    e.classList.remove("active-dropdown");
+  } else {
+    dropdownContent.style.maxHeight = dropdownContent.scrollHeight + "px";
+    e.classList.add("active-dropdown");
+  }
+}
